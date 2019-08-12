b71ccf7ad72ef841a5604312afc967c51eac1f0d
onnsui <ryo.sato@scouter.co.jp>
2019-08-13 00:30:40 +0900

    Taskモデルを返すように

--- app/Repositories/Eloquent/TaskRepositoryEloquent.php
+++ app/Repositories/Eloquent/TaskRepositoryEloquent.php
@@ -11,7 +11,7 @@ class TaskRepositoryEloquent implements TaskRepository
     public function whereSearch(string $search = null)
     {
         if (!$search) {
-            return $this;
+            return Task::all();
         }
         $tasks = Task::where('title', 'like', "%${search}%")
             ->get();
