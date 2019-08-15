88f3598775795678b9a652bd3b452e759eda8c58
onnsui <ryo.sato@scouter.co.jp>
2019-08-15 22:48:04 +0900

    検索条件付き一覧GETのテスト作成

--- tests/Feature/TaskControllerTest.php
+++ tests/Feature/TaskControllerTest.php
@@ -19,4 +19,26 @@ class TaskControllerTest extends TestCase
         $res = $this->getJson(route('task-index'));
         $res->assertStatus(200);
     }
+
+    /**
+     * @test
+     */
+    public function 検索条件付き一覧取得のテスト()
+    {
+        $tasks = factory(Task::class, 2)->create([
+            'title' => '12345678',
+        ]);
+
+        //　検索でヒットしないCompany
+        factory(Task::class, 2)->create([
+            'title' => 'zzzzzzzz'
+        ]);
+
+        $res = $this->getJson(route('task-index', [
+            'search' => '12345678'
+        ]));
+        $res->assertStatus(200);
+
+        $this->assertEquals($tasks->count(), count(json_decode($res->content(), true)));
+    }
 }
