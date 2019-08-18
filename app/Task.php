fcfe736bc6b294145dde6d81d3e4762578409b58
onnsui <ryo.sato@scouter.co.jp>
2019-08-18 21:52:13 +0900

    Taskモデルにステータスのリストを追加

--- app/Task.php
+++ app/Task.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    const STATUS_LIST = [
+        '1' => 'work',
+        '2' => 'private',
+        '3' => 'other'
+    ];
 }
