0855848ca14477e9b69b6745456574dd6a506cc0
onnsui <ryo.sato@scouter.co.jp>
2019-08-18 21:50:51 +0900

    TaskFactoryのstatusを修正

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -9,10 +9,9 @@ $factory->define(Task::class, function (Faker $faker) {
         'title' => $faker->word,
         'content' => $faker->word,
         'due_date' => $faker->dateTime,
-        'status' => $faker->randomDigit,
+        'status' => array_rand(Task::STATUS_LIST),
         'category_id'  => function () {
             return factory(Category::class)->create()->id;
         },
-//        'category_id' => $faker->randomElement(array_keys(config('master.category'))),
     ];
 });
