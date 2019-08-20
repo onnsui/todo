f52b4057325e93b173d701c85a6ee418e13acd81
onnsui <ryo.sato@scouter.co.jp>
2019-08-20 21:40:19 +0900

    taskとカテゴリーの中間テーブルを作成

---
+++ database/migrations/2019_08_18_000001_create_task_category_ref_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTaskCategoryRefTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('task_category_ref', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('task_id')->unsigned();
+            $table->foreign('task_id')->references('id')->on('tasks');
+            $table->integer('category_id')->unsigned();
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('task_category_ref');
+    }
+}
