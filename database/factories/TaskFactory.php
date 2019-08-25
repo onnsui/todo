e0fb826dfaecc75badb0f4343965da0688c4dd92
onnsui <ryo.sato@scouter.co.jp>
2019-08-25 22:10:31 +0900

    factoryを修正

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -2,11 +2,15 @@
 
 use App\Task;
 use App\Category;
+use App\User;
 use Carbon\Carbon;
 use Faker\Generator as Faker;
 
 $factory->define(Task::class, function (Faker $faker) {
     return [
+        'user_id' => function() {
+            return factory(User::class)->create()->id;
+        },
         'title' => $faker->word,
         'content' => $faker->word,
         'due_date' => Carbon::tomorrow()->toDateTimeString(),
