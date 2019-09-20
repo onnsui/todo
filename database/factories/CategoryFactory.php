818421a1e2a38f8d96719fc5f4ac43bb4eed784a
onnsui <ryo.sato@scouter.co.jp>
2019-09-20 21:37:37 +0900

    categoryファクトリーの変更

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -1,10 +1,14 @@
 <?php
 
+use App\User;
 use App\Category;
 use Faker\Generator as Faker;
 
 $factory->define(Category::class, function (Faker $faker) {
     return [
+        'user_id' => function () {
+            return factory(User::class)->create()->id;
+        },
         'title' => $faker->word,
     ];
 });
